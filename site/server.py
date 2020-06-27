595171e6716802fc868685d59debe3d063e10441
hi_melnikov <melnikovam10@gmail.com>
2020-06-27 19:01:53 +0300

    all without ds

--- site/server.py
+++ site/server.py
@@ -16,5 +16,12 @@ def graph():
     fig.write_html("templates/gen/graph.html")
     return redirect(url_for('main'))
 
+@app.route('/search')
+def search_get():
+    id = request.args.get('id')
+    if not id:
+        return redirect(url_for('main'))
+    return render_template("search.html", id = id)
+
 if __name__ == "__main__":
     app.run(debug = True)
