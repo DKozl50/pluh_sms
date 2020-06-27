f6c3f6e27f4dff7da44b4e3897ef338ab8bfde62
ivan7022 <1vanmoshkovv@mail.ru>
2020-06-27 09:19:09 +0000

    some fixes

--- features/nearest.py
+++ features/nearest.py
@@ -6,7 +6,7 @@ def PredictCardHolder(data):
     scl = StandardScaler()
     svd = PCA(n_components=5)
     new_features = svd.fit_transform(scl.fit_transform(data.drop(['CardHolder', 'response_att', 'group'], axis=1)))
-    
+    new_features = new_features - new_features.mean()
     knn = KNeighborsClassifier()
     knn.fit(new_features[np.where(data['group']==1)], data[data['group']==1]['CardHolder'])
     predicted_class = knn.predict(new_features[np.where(data['group']==0)])  
